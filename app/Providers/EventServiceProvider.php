e017117614d415c443cecb17b69495f3252c748f
leoroxaslr <leoroxas.lr@gmail.com>
2023-10-25 09:27:34 +0800

    final

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -6,6 +6,7 @@ use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Event;
+use Illuminate\Support\Facades\URL;
 
 class EventServiceProvider extends ServiceProvider
 {
@@ -23,9 +24,11 @@ class EventServiceProvider extends ServiceProvider
     /**
      * Register any events for your application.
      */
-    public function boot(): void
+ 
+
+    public function boot()
     {
-        //
+           URL::forceScheme('https');
     }
 
     /**
